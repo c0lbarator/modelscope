94158dad5e5ba0626566641bdded2cbe63553bc7
c0lbarator <57714998+c0lbarator@users.noreply.github.com>
2023-02-21 15:29:46 +0300

    Update __init__.py

--- modelscope/models/audio/ans/__init__.py
+++ modelscope/models/audio/ans/__init__.py
@@ -3,21 +3,4 @@ from typing import TYPE_CHECKING
 
 #from modelscope.utils.import_utils import LazyImportModule
 
-
-if TYPE_CHECKING:
-    from .frcrn import FRCRNDecorator
-
-else:
-    _import_structure = {
-        'frcrn': ['FRCRNDecorator'],
-    }
-
-    import sys
-
-    sys.modules[__name__] = LazyImportModule(
-        __name__,
-        globals()['__file__'],
-        _import_structure,
-        module_spec=__spec__,
-        extra_objects={},
-    )
+from .frcrn import FRCRNDecorator
