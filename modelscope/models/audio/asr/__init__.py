568a629cc77189d2dd9f639f72701891f9f5ddcc
c0lbarator <57714998+c0lbarator@users.noreply.github.com>
2023-02-21 15:30:05 +0300

    Update __init__.py

--- modelscope/models/audio/asr/__init__.py
+++ modelscope/models/audio/asr/__init__.py
@@ -4,21 +4,6 @@ from typing import TYPE_CHECKING
 #from modelscope.utils.import_utils import LazyImportModule
 
 
-if TYPE_CHECKING:
-    from .generic_automatic_speech_recognition import GenericAutomaticSpeechRecognition
+from .generic_automatic_speech_recognition import GenericAutomaticSpeechRecognition
 
-else:
-    _import_structure = {
-        'generic_automatic_speech_recognition':
-        ['GenericAutomaticSpeechRecognition'],
-    }
 
-    import sys
-
-    sys.modules[__name__] = LazyImportModule(
-        __name__,
-        globals()['__file__'],
-        _import_structure,
-        module_spec=__spec__,
-        extra_objects={},
-    )
