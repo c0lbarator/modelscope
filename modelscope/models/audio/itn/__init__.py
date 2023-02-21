3d6f3756db6dfe59c800d294d04efdfeb811258d
c0lbarator <57714998+c0lbarator@users.noreply.github.com>
2023-02-21 15:30:22 +0300

    Update __init__.py

--- modelscope/models/audio/itn/__init__.py
+++ modelscope/models/audio/itn/__init__.py
@@ -4,20 +4,5 @@ from typing import TYPE_CHECKING
 #from modelscope.utils.import_utils import LazyImportModule
 
 
-if TYPE_CHECKING:
-    from .generic_inverse_text_processing import GenericInverseTextProcessing
+from .generic_inverse_text_processing import GenericInverseTextProcessing
 
-else:
-    _import_structure = {
-        'generic_inverse_text_processing': ['GenericInverseTextProcessing'],
-    }
-
-    import sys
-
-    sys.modules[__name__] = LazyImportModule(
-        __name__,
-        globals()['__file__'],
-        _import_structure,
-        module_spec=__spec__,
-        extra_objects={},
-    )
